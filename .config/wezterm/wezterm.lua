504a45b40f58e4202740f2abb30993b7e16273cd
Alessio Biancalana <dottorblaster@gmail.com>
2021-10-20 14:49:02 +0200

    Add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,19 @@
+local wezterm = require 'wezterm';
+return {
+  font = wezterm.font("Fira Code", { weight="Medium" }),
+  font_size = 8.0,
+  window_padding = {
+    left = 5,
+    right = 5,
+    top = 5,
+    bottom = 5,
+  },
+  colors = {
+    foreground = "#deddda",
+    background = "#1d1f21",
+    ansi = {"#1d1f21", "#cc6666", "#b5bd68", "#f0c674", "#81a2be", "#b294bb", "#8abeb7", "#c5c8c6"},
+    brights = {"#3d3846", "#cc6666", "#b5bd68", "#f0c674", "#81a2be", "#b294bb", "#8abeb7", "#ffffff"},
+  },
+  exit_behavior = "Close",
+  hide_tab_bar_if_only_one_tab = true,
+}
