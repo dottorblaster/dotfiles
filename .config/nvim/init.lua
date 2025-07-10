808a092527d438420547317730a9a56218b81221
Alessio Biancalana <alessio@dottorblaster.it>
2025-07-10 10:58:41 +0200

    config: add neoformat and LSP diagnostics to nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -43,6 +43,8 @@ require("lazy").setup({
   "RRethy/nvim-base16",
   "hoob3rt/lualine.nvim",
   "lewis6991/gitsigns.nvim",
+  "dgagn/diagflow.nvim",
+  "sbdchd/neoformat",
   "Pocco81/true-zen.nvim",
   {"saecki/crates.nvim", tag = "v0.3.0" },
   "kyazdani42/nvim-web-devicons",
@@ -70,6 +72,8 @@ vim.opt.nu = true
 vim.opt.splitright = true
 
 vim.opt.termguicolors = true
+
+vim.g.neoformat_try_node_exe = 1
 
 vim.cmd('let base16colorspace=256')
 
@@ -166,7 +170,7 @@ cmp.setup {
 
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = {"rust_analyzer", "elixirls", "tsserver", "gopls"}
+  ensure_installed = {"rust_analyzer", "elixirls", "ts_ls", "gopls"}
 })
 
 local on_attach = function(client, bufnr)
@@ -198,7 +202,7 @@ local lsp_flags = {
   debounce_text_changes = 300,
 }
 
-require('lspconfig')['tsserver'].setup{
+require('lspconfig')['ts_ls'].setup{
     on_attach = on_attach,
     flags = lsp_flags
 }
@@ -371,6 +375,41 @@ require("neo-tree").setup({
   window = {
     width = 30,
   },
+})
+
+require('diagflow').setup({
+    enable = true,
+    max_width = 60,  -- The maximum width of the diagnostic messages
+    max_height = 10, -- the maximum height per diagnostics
+    severity_colors = {  -- The highlight groups to use for each diagnostic severity level
+        error = "DiagnosticFloatingError",
+        warning = "DiagnosticFloatingWarn",
+        info = "DiagnosticFloatingInfo",
+        hint = "DiagnosticFloatingHint",
+    },
+    format = function(diagnostic)
+      return diagnostic.message
+    end,
+    gap_size = 1,
+    scope = 'cursor', -- 'cursor', 'line' this changes the scope, so instead of showing errors under the cursor, it shows errors on the entire line.
+    padding_top = 0,
+    padding_right = 0,
+    text_align = 'right', -- 'left', 'right'
+    placement = 'top', -- 'top', 'inline'
+    inline_padding_left = 0, -- the padding left when the placement is inline
+    update_event = { 'DiagnosticChanged', 'BufReadPost' }, -- the event that updates the diagnostics cache
+    toggle_event = { }, -- if InsertEnter, can toggle the diagnostics on inserts
+    show_sign = false, -- set to true if you want to render the diagnostic sign before the diagnostic message
+    render_event = { 'DiagnosticChanged', 'CursorMoved' },
+    border_chars = {
+      top_left = "┌",
+      top_right = "┐",
+      bottom_left = "└",
+      bottom_right = "┘",
+      horizontal = "─",
+      vertical = "│"
+    },
+    show_borders = true,
 })
 
 vim.api.nvim_create_autocmd("VimEnter", {
