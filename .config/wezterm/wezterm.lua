5065868931a79d4389777e2b9d31a4b6f83379b4
Alessio Biancalana <alessio@dottorblaster.it>
2025-11-09 12:23:15 +0100

    config: fix wezterm cursor

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -9,11 +9,19 @@ return {
     bottom = 5,
   },
   colors = {
+    tab_bar = {
+      background = '#1d1f21',
+    },
     foreground = "#deddda",
     background = "#1d1f21",
+    compose_cursor = "#deddda",
+    cursor_bg = "#deddda",
+    cursor_fg = "#1d1f21",
+    cursor_border = "#1d1f21",
     ansi = {"#1d1f21", "#cc6666", "#b5bd68", "#f0c674", "#81a2be", "#b294bb", "#8abeb7", "#c5c8c6"},
     brights = {"#3d3846", "#cc6666", "#b5bd68", "#f0c674", "#81a2be", "#b294bb", "#8abeb7", "#ffffff"},
   },
   exit_behavior = "Close",
   hide_tab_bar_if_only_one_tab = true,
+  use_fancy_tab_bar = false,
 }
